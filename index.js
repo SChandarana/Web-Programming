623d87c8b5cb88b9d886f78c225f9fec45802b8d
SChandarana <44266225+SChandarana@users.noreply.github.com>
2019-01-26 13:59:07 +0000

    basic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var express = require('express')
-var app = express()
+"use strict";
+var express = require('express');
+var app = express();
 
 app.get('/', function(req,resp){
 	resp.send("Hello world")
